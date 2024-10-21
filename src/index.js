7c908439a2e75f4b3d9583ae50d96633312400ea
AstroHammer <jpalato101@gmail.com>
2024-10-21 16:13:23 -0700

    probably not a good idea

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-{\rtf1}
+import { gsap } from "gsap";
+
+/* The following plugin is a Club GSAP perk */
+import { SplitText } from "gsap/SplitText";
+
+
+gsap.registerPlugin(SplitText);
+
+
+var split = new SplitText("h1", {type: "chars"});
+
+gsap.from(split.chars, {
+    duration: 1,
+    y: 100,
+    autoAlpha: 0,
+    stagger: 0.05
+});
