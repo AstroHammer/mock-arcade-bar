e3a8b56019c1f43274f45f1c1093baae7fd53dd7
AstroHammer <jpalato101@gmail.com>
2024-10-25 12:40:56 -0700

    filling structure out

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import { gsap } from "gsap";
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-/* The following plugin is a Club GSAP perk */
-// import { SplitText } from "gsap/SplitText";
-// gsap.registerPlugin(SplitText);
 
-ScrollSmoother.create({
-    wrapper: "#smooth-wrapper",
-    content: "#smooth-content",
-    smooth: 1,
-    effects: true,
-  });
 
 import Rellax from "rellax";
 
