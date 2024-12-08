320c3769049b81e465dd2d2d8939cfe8c3805e3f
unknown <jpalato101@gmail.com>
2024-12-08 10:07:30 -0800

    rebuild

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { gsap } from "gsap";
 
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(DrawSVGPlugin);
 gsap.registerPlugin(ScrollTrigger);
 
